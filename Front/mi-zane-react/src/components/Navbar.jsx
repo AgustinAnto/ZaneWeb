c6c434e3a3e881f6df9b334f11636d05d7f8d819
AgustinAnto <aguss2016z@gmail.com>
2024-11-07 11:42:48 -0300

    7/11

--- Front/mi-zane-react/src/components/Navbar.jsx
+++ Front/mi-zane-react/src/components/Navbar.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const Navbar = ({  currentUser, setShowLogin, setShowSignup, onLogout }) => {
     return (
         <div>
-
+            {currentUser ? (
+                    <button className="nav-button" onClick={onLogout}>Logout</button>
+                ) : (
+                    <>
+                        <button className="nav-button" onClick={() => setShowLogin(true)}>Login</button>
+                        <button className="nav-button" onClick={() => setShowSignup(true)}>Sign Up</button>
+                    </>
+                )}
         </div>
     )
 }
